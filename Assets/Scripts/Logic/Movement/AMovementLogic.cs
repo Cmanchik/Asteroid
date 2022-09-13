4d1c6dba301b5ae562c4f6e37721f001b4700e6f
Cmanchik <aksenova1648@gmail.com>
2022-09-13 14:52:30 +0400

    Логика передвижения

---
+++ Assets/Scripts/Logic/Movement/AMovementLogic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Logic.Movement
+{
+    public abstract class AMovementLogic
+    {
+        /// <summary>
+        /// Велечина ускорения
+        /// </summary>
+        protected readonly float acceleration;
+
+        /// <summary>
+        /// Текущее ускорение
+        /// </summary>
+        protected float currentAcceleration;
+
+        /// <summary>
+        /// Макс допустимое ускорение
+        /// </summary>
+        protected readonly float maxAcceleration;
+
+        /// <summary>
+        /// Положение объекта
+        /// </summary>
+        protected readonly Transform transform;
+     
+        /// <summary>
+        /// Текущая мгновенная скорость
+        /// </summary>
+        public float CurrentSpeed { get { return currentAcceleration; } }
+
+        /// <summary>
+        /// Конструктор логики перемещения
+        /// </summary>
+        /// <param name="acceleration">Величина с которой будет ускорятся объект</param>
+        /// <param name="maxAcceleration">Макс величина ускорения</param>
+        /// <param name="transform">Положение объекта</param>
+        public AMovementLogic(float acceleration, float maxAcceleration, Transform transform)
+        {
+            this.acceleration = acceleration;
+            this.maxAcceleration = maxAcceleration;
+            this.transform = transform;
+        }
+
+        /// <summary>
+        /// Логика передвижения
+        /// </summary>
+        public abstract void Move();
+    }
+}
---
+++ Assets/Scripts/Logic/Movement/StarshipMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Logic.Movement
+{
+    public class StarshipMovement : AMovementLogic
+    {
+        /// <summary>
+        /// Сила сопротивления движению
+        /// </summary>
+        private readonly float movementResistance;
+
+        /// <summary>
+        /// Конструктор движения коробля
+        /// </summary>
+        /// <param name="movementResistance">Сила сопротивления движению</param>
+        /// <param name="acceleration">Величина с которой будет ускорятся объект</param>
+        /// <param name="maxAcceleration">Макс величина ускорения</param>
+        /// <param name="transform">Положение объекта</param>
+        public StarshipMovement(float movementResistance, float acceleration, float maxAcceleration, Transform transform) : 
+            base(acceleration, maxAcceleration, transform)
+        {
+            this.movementResistance = movementResistance;
+        }
+
+        public override void Move()
+        {
+            currentAcceleration += currentAcceleration + acceleration >= maxAcceleration ? maxAcceleration : acceleration;
+            transform.Translate(Vector2.up * currentAcceleration);
+        }
+
+        /// <summary>
+        /// Логика затухания движения
+        /// </summary>
+        public void ResistMovement()
+        {
+            currentAcceleration -= currentAcceleration - movementResistance <= 0 ? currentAcceleration : movementResistance;
+        }
+    }
+}
