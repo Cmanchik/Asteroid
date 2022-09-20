51066ccec57c5d5935077d0d28940d2480cd67ad
Cmanchik <aksenova1648@gmail.com>
2022-09-20 11:32:10 +0400

    Реализация движения вперед

---
+++ Assets/Scripts/UI/ShipIndicators.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Performance.Movement;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class ShipIndicators : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text speedField;
+
+        [Space]
+
+        [SerializeField] private MovementScript movementLogic;
+
+        private void Update()
+        {
+            speedField.text = movementLogic.CurrentSpeed.ToString();
+        }
+    }
+}
