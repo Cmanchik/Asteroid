4c493544fef8d3184ba7a7c1fa141a6e2298eed6
Cmanchik <aksenova1648@gmail.com>
2022-09-22 16:21:30 +0400

    Телепортация при выходе за экран

---
+++ Assets/Scripts/Logic/Border/BorderTeleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Logic.Border
+{
+    public class BorderTeleport
+    {
+        private float sizeX;
+        private float sizeY;
+
+        public BorderTeleport(float sizeX, float sizeY)
+        {
+            this.sizeX = sizeX / 2;
+            this.sizeY = sizeY / 2;
+        }
+
+        public Vector2 Teleport(Vector2 posCollision)
+        {
+            if (posCollision.x <= -sizeX) return new Vector2(2 * sizeX + posCollision.x, posCollision.y);
+            if (posCollision.x >= sizeX) return new Vector2(posCollision.x - (2 * sizeX), posCollision.y);
+            if (posCollision.y <= -sizeY) return new Vector2(posCollision.x, 2 * sizeY + posCollision.y);
+            if (posCollision.y >= sizeY) return new Vector2(posCollision.x, posCollision.y - (2 * sizeY));
+
+            return posCollision;
+        }
+    }
+}
---
+++ Assets/Scripts/Performance/Border/BorderTeleportScript.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Logic.Border;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Performance.Border
+{
+    public class BorderTeleportScript : MonoBehaviour
+    {
+        BorderTeleport border;
+
+        private void Awake()
+        {
+            BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+            border = new BorderTeleport(boxCollider.size.x, boxCollider.size.y);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            collision.gameObject.transform.position = border.Teleport(collision.gameObject.transform.position);
+        }
+    }
+}
