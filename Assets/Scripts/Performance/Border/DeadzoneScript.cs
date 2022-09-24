24b77e96fde1b8c172711af638f88e39d64772e4
Cmanchik <aksenova1648@gmail.com>
2022-09-24 14:49:31 +0400

    Создание уничтожающей зоны для астроилов и снарядов

---
+++ Assets/Scripts/Performance/Border/DeadzoneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Performance.Border
+{
+    /// <summary>
+    /// Класс уничтожающие выбранные объекты, если они выходят за колладер зоны
+    /// </summary>
+    public class DeadzoneScript : MonoBehaviour
+    {
+        /// <summary>
+        /// Объекты подлижащие уничтожению
+        /// </summary>
+        [SerializeField] private LayerMask layerMask;
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (layerMask == (layerMask | (1 << collision.gameObject.layer)))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+
+    }
+}
