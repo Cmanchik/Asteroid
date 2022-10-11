f043e5eba74d281fa73705a6294319c8208ae3cb
Cmanchik <aksenova1648@gmail.com>
2022-10-11 08:54:24 +0400

    Отобрадение полного набора инворации о корабле

--- Assets/Scripts/UI/ShipIndicators.cs
+++ Assets/Scripts/UI/ShipIndicators.cs
@@ -7,14 +7,35 @@ namespace Assets.Scripts.UI
     public class ShipIndicators : MonoBehaviour
     {
         [SerializeField] private TMP_Text speedField;
+        [SerializeField] private TMP_Text coordinateField;
+        [SerializeField] private TMP_Text angleField;
+
+        private string speedText;
+        private string coordinateText;
+        private string angleText;
 
         [Space]
 
         [SerializeField] private StartshipMovementScript movementLogic;
+        [SerializeField] private Transform body;
+        [SerializeField] private Transform starShipBody;
+
+        private new Transform transform;
+
+        private void Awake()
+        {
+            transform = GetComponent<Transform>();
+
+            speedText = speedField.text;
+            coordinateText = coordinateField.text;
+            angleText = angleField.text;
+        }
 
         private void Update()
         {
-            speedField.text = movementLogic.CurrentSpeed.ToString();
+            speedField.text = string.Format(speedText, movementLogic.CurrentSpeed.ToString());
+            coordinateField.text = string.Format(coordinateText, body.position.ToString());
+            angleField.text = string.Format(angleText, starShipBody.eulerAngles.z.ToString()); 
         }
     }
 }
